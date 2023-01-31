45831f68157154c389d5b23ac9ae31bcc0b722d5
clyde <magiclyde@163.com>
2023-01-31 18:18:15 +0800

    with errgroup

--- downloader_test.go
+++ downloader_test.go
@@ -9,17 +9,17 @@ package downloader
 
 import (
 	"context"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"golang.org/x/sync/semaphore"
 	"io/ioutil"
 	"log"
 	"os"
-	"sync"
 	"testing"
-
-	"golang.org/x/sync/semaphore"
 )
 
 const (
-	limit  = 3 // 下载站点有连接数限制，因而设定同時运行的 goroutine 上限
+	limit  = 3 // 并发下载的 goroutine 上限, 因为下载站点有连接数限制
 	weight = 1 // 每个 goroutine 获取信号量资源的权重
 )
 
@@ -31,32 +31,35 @@ func TestDownloader_Run(t *testing.T) {
 	}
 	defer os.RemoveAll(tmpDir)
 	//t.Log(tmpDir)
-
-	s := semaphore.NewWeighted(limit)
-	var wg sync.WaitGroup
 	options := []Option{
 		WithTotalPart(5),
 		WithOutputDir(tmpDir),
 	}
+
+	eg, _ := errgroup.WithContext(context.Background())
+	sem := semaphore.NewWeighted(limit)
 	for _, url := range []string{
+		"http://xxx", // invalid url
 		"http://dl.magiclyde.com/xhprof-0.9.4.tgz",                       // Accept-Ranges: none
 		"http://dl.magiclyde.com/mindoc-src.tar.gz",                      // Accept-Ranges: none, big file
 		"http://dl.magiclyde.com/php-7.2.30.tar.gz",                      // Accept-Ranges: bytes
 		"http://dl.magiclyde.com/openresty-1.19.9.1.tar.gz",              // Accept-Ranges: bytes
 		"http://dl.magiclyde.com/tor-browser-linux64-9.0.1_zh-CN.tar.xz", // Accept-Ranges: bytes
 	} {
-		wg.Add(1)
-		go func(url string) {
-			defer wg.Done()
-			defer s.Release(weight)
-			s.Acquire(context.Background(), weight)
+		url := url
+		eg.Go(func() error {
+			defer sem.Release(weight)
+			if err := sem.Acquire(context.Background(), weight); err != nil {
+				return fmt.Errorf("failed to acquire semaphore: %s", err.Error())
+			}
 			downloader := NewDownloader(url, options...)
 			if err := downloader.Run(); err != nil {
-				t.Errorf("downloader.Run().err: %s", err.Error())
-				return
+				return fmt.Errorf("downloader.Run got err: %s", err.Error())
 			}
-			t.Logf("ok, %s", url)
-		}(url)
+			return nil
+		})
 	}
-	wg.Wait()
+	if err := eg.Wait(); err != nil {
+		t.Log(err)
+	}
 }
