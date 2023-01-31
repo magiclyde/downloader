33561f4bb529b7e974df3441e2d67e33d2ba1182
clyde <magiclyde@163.com>
2023-01-31 15:50:24 +0800

    use semaphore 控制最多运行的 goroutine 数量

--- downloader_test.go
+++ downloader_test.go
@@ -8,11 +8,19 @@
 package downloader
 
 import (
+	"context"
 	"io/ioutil"
 	"log"
 	"os"
 	"sync"
 	"testing"
+
+	"golang.org/x/sync/semaphore"
+)
+
+const (
+	limit  = 3 // 下载站点有连接数限制，因而设定同時并行运行的 goroutine 上限
+	weight = 1 // 每个 goroutine 获取信号量资源的权重
 )
 
 func TestDownloader_Run(t *testing.T) {
@@ -24,24 +32,29 @@ func TestDownloader_Run(t *testing.T) {
 	defer os.RemoveAll(tmpDir)
 	//t.Log(tmpDir)
 
+	s := semaphore.NewWeighted(limit)
 	var wg sync.WaitGroup
 	options := []Option{
 		WithTotalPart(5),
 		WithOutputDir(tmpDir),
 	}
 	for _, url := range []string{
-		"http://dl.magiclyde.com/mindoc-src.tar.gz",                // Accept-Ranges: none, big file
-		"http://dl.magiclyde.com/xhprof-0.9.4.tgz",                 // Accept-Ranges: none
-		"http://dl.magiclyde.com/agentzh-nginx-tutorials-zhcn.pdf", // Accept-Ranges: bytes
+		"http://dl.magiclyde.com/xhprof-0.9.4.tgz",                       // Accept-Ranges: none
+		"http://dl.magiclyde.com/mindoc-src.tar.gz",                      // Accept-Ranges: none, big file
+		"http://dl.magiclyde.com/php-7.2.30.tar.gz",                      // Accept-Ranges: bytes
+		"http://dl.magiclyde.com/openresty-1.19.9.1.tar.gz",              // Accept-Ranges: bytes
+		"http://dl.magiclyde.com/tor-browser-linux64-9.0.1_zh-CN.tar.xz", // Accept-Ranges: bytes
 	} {
 		wg.Add(1)
 		go func(url string) {
-			defer wg.Done()
+			s.Acquire(context.Background(), weight)
 			downloader := NewDownloader(url, options...)
 			if err := downloader.Run(); err != nil {
 				t.Errorf("downloader.Run().err:%s", err.Error())
 			}
 			t.Logf("ok, %s", url)
+			s.Release(weight)
+			wg.Done()
 		}(url)
 	}
 	wg.Wait()
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/schollz/progressbar/v3 v3.8.3
 	github.com/urfave/cli/v2 v2.3.0
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sync v0.1.0
 	golang.org/x/sys v0.0.0-20211025201205-69cdffdb9359 // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 )
